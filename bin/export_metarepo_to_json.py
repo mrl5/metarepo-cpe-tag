224483215a1c0e98a1373854d23315a091ab991f
mrl5 <31549762+mrl5@users.noreply.github.com>
2020-12-06 15:14:22 +0100

    parallel friendly formt of metarepo dump

--- bin/export_metarepo_to_json.py
+++ bin/export_metarepo_to_json.py
@@ -76,46 +76,55 @@ def get_recent_stable_branch(kit):
     return prime_branches_names.pop()
 
 
-async def get_catpkgs(categories, kit_name, commit):
-    for category in categories:
-        catpkgs_instance = hub.metarepo2json.factory.get_catpkgs_instance(
-            kit=kit_name, source=source
-        )
-        await setup_catpkgs_instance(catpkgs_instance, category["name"], commit)
-        category["packages"] = await catpkgs_instance.get_result()
-    return categories
+async def get_catpkgs(category, kit_name, commit):
+    catpkgs_instance = hub.metarepo2json.factory.get_catpkgs_instance(
+        kit=kit_name, source=source
+    )
+    await setup_catpkgs_instance(catpkgs_instance, category["name"], commit)
+    return await catpkgs_instance.get_result()
 
 
-async def create_kit_file(source, workdir, kit):
-    print(kit)
+async def dump_category(category, kit_name, commit, kitdir):
+    catpkgs = await get_catpkgs(category, kit_name, commit)
+    cat_path = Path(kitdir / f"{category['name']}.json")
+    cat_path.touch(mode=0o664)
+    with open(cat_path, "w") as f:
+        f.write("")
+    for catpkg in catpkgs:
+        with open(cat_path, "a") as f:
+            f.write(json.dumps(catpkg) + "\n")
+
+
+async def dump_kit(source, workdir, kit):
+    kit_name = kit['name']
+    kitdir = workdir / kit_name
+    Path.mkdir(kitdir, parents=True, exist_ok=True)
     branch_name = get_recent_stable_branch(kit)
     branch = list(filter(lambda x: x["name"] == branch_name, kit["branches"])).pop()
     commit = branch["sha1"]
-    kit_path = Path(workdir / f"{kit['name']}.json")
-    kit_path.touch(mode=0o664)
     cats_instance = hub.metarepo2json.factory.get_categories_instance(
-        kit=kit["name"], source=source
+        kit=kit_name, source=source
     )
     await setup_cats_instance(cats_instance, commit)
     categories = await cats_instance.get_result()
-    catpkgs = await get_catpkgs(categories, kit["name"], commit)
-    branch["catpkgs"] = catpkgs
-    kit["branches"] = [branch]
-    with open(kit_path, "w") as f:
-        f.write(json.dumps(kit))
+    with open(kitdir / "head.sha1", "w") as f:
+        f.write(commit + "\n")
+    for category in categories:
+        await dump_category(category, kit_name, commit, kitdir)
 
 
-async def create_kit_files(source, workdir):
+async def dump_metarepo(source, workdir):
     kits_instance = hub.metarepo2json.factory.get_kits_instance(source=source)
     await setup_kits_instance(kits_instance)
     kits = await kits_instance.get_result()
     done_tasks, _ = await asyncio.wait(
-        # for some reason acts synchronousely
-        tuple(create_kit_file(source, workdir, kit) for kit in kits)
+        # issue-8: for some reason acts synchronousely
+        tuple(dump_kit(source, workdir, kit) for kit in kits)
     )
 
 
 if __name__ == "__main__":
+    logging.disable(level=logging.INFO)
     source = hub.OPT.metarepo2json.data_source
     exit_on_invalid_source(source)
     expected_errors = [
@@ -127,8 +136,7 @@ if __name__ == "__main__":
     ]
     try:
         workdir = Path.home() / hub.OPT.metarepo2json.output_dir
-        Path.mkdir(workdir, parents=True, exist_ok=True)
-        errors = asyncio.run(create_kit_files(source, workdir))
+        errors = asyncio.run(dump_metarepo(source, workdir))
         if errors:
             process_errors(errors)
             sys.exit(1)
