df0808440839e94d8976e35e2bc2aae1fc0a9be4
mrl5 <31549762+mrl5@users.noreply.github.com>
2020-12-06 20:52:55 +0100

    issue-5: logic to query CPE Match Feed

--- cpe_tag/conf.py
+++ cpe_tag/conf.py
@@ -1,8 +1,11 @@
+from pathlib import Path
+
+
 CONFIG = {
-    "package": {"default": None, "help": "package name from meta-repo",},
-    "version": {"default": None, "help": "version in funtoo format (e.g. 1.2_p1-r1)",},
+    "package": {"default": None, "help": "package name from meta-repo"},
+    "version": {"default": None, "help": "version in funtoo format (e.g. 1.2_p1-r1)"},
     "cpe_match_feed": {
-        "default": "feeds/json/nvdcpematch-1.0.json.gz",
+        "default": str(Path.home() / "feeds/json/nvdcpematch-1.0.json.gz"),
         "help": "location of NVD CPE Match Feed on filesystem",
     },
 }
---
+++ cpe_tag/cpe_tag/searchers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import gzip
+from re import search
+
+
+def get_cpe_uri_from_json_line(hub, json_line) -> str:
+    return ':'.join(json_line.split(':')[1:]).strip().replace('"', "")
+
+
+def query_cpe_match(hub, pattern) -> list:
+    feed_loc = hub.OPT.cpe_tag.cpe_match_feed
+    matches = []
+    with gzip.open(feed_loc, "rt") as f:
+        for line in f:
+            s = search(pattern, line)
+            if s is not None:
+                matches.append(get_cpe_uri_from_json_line(hub, s.string))
+    return matches
