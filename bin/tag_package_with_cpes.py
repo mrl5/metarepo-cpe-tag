2517c285f6725a17f9becbb050cb7289b644b2a1
mrl5 <31549762+mrl5@users.noreply.github.com>
2020-12-12 16:59:35 +0100

    bugfix: github issue #24

--- bin/tag_package_with_cpes.py
+++ bin/tag_package_with_cpes.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import json
+from pathlib import Path
 
 import pop.hub
 
@@ -11,11 +12,21 @@ hub.pop.sub.add(dyne_name="cpe_tag")
 hub.cpe_tag.init.cli()
 
 
-def main():
+def throw_on_invalid_feed(feed_loc):
+    try:
+        assert Path(feed_loc).is_file() is True
+    except AssertionError:
+        raise OSError(f"{feed_loc}: no such file")
+
+
+def run():
     package = json.loads(hub.OPT.cpe_tag.package_json)
+    throw_on_invalid_feed(hub.OPT.cpe_tag.cpe_match_feed)
     serialized = hub.cpe_tag.serializers.serialize_package_json(package)
     tagged = hub.cpe_tag.generators.tag_package_with_cpes(serialized)
-    return json.dumps(tagged)
+    print(json.dumps(tagged))
 
 
-print(main())
+if __name__ == "__main__":
+    from sys import exit
+    exit(run())
--- cpe_tag/cpe_tag/contracts/searchers.py
+++ cpe_tag/cpe_tag/contracts/searchers.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
-from re import search
-from shlex import quote
-
 
 async def sig_query_cpe_match(hub, quasi_cpe: str, feed=None):
     pass
@@ -13,28 +9,4 @@ async def call_query_cpe_match(hub, ctx):
     quasi_cpe = ctx.args[1]
     if quasi_cpe is None:
         return []
-    return await ctx.func(*ctx.args, **ctx.kwargs)
-
-
-async def sig_get_feed(hub, feed_loc: str, quasi_cpe: str):
-    pass
-
-
-async def pre_get_feed(hub, ctx):
-    try:
-        feed_loc = ctx.args[1]
-        assert Path(feed_loc).exists() is True
-    except AssertionError:
-        raise hub.cpe_tag.errors.SearcherError(f"{feed_loc}: No such file or directory")
-
-    try:
-        quasi_cpe = ctx.args[2]
-        assert search(r"^[^:].*:[^:].+:[^:].+:[^:].*$", quasi_cpe) is not None
-    except AssertionError:
-        raise hub.cpe_tag.errors.SearcherError("invalid format of quasi_cpe")
-
-
-async def call_get_feed(hub, ctx):
-    ctx.args[1] = quote(ctx.args[1])
-    ctx.args[2] = quote(ctx.args[2])
     return await ctx.func(*ctx.args, **ctx.kwargs)
